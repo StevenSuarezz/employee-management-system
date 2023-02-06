d4bff7a8f1fd1ba20c0f176b2ba47672206ebcaf
Steven <suarezsteven30@gmail.com>
2023-02-06 18:46:22 -0500

    Get by ID and update endpoints created

--- employee-manager-backend/src/main/java/com/suarez/employeemanagerbackend/controller/EmployeeController.java
+++ employee-manager-backend/src/main/java/com/suarez/employeemanagerbackend/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.suarez.employeemanagerbackend.controller;
 
+import com.suarez.employeemanagerbackend.exception.ResourceNotFoundException;
 import com.suarez.employeemanagerbackend.model.Employee;
 import com.suarez.employeemanagerbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -17,6 +19,22 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        if(employeeOptional.isEmpty()) {
+            throw new ResourceNotFoundException("Employee with Id: " + id + " does not exist.");
+        } else {
+            return employeeOptional.get();
+        }
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        //TODO: Add validation
+        return employeeRepository.save(employee);
     }
 
     @PostMapping("/employees")
